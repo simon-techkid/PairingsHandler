46006d00bb86ac1bf120e218a0b9d29d83eaf4fd
TheBoyLeastLikelyTo <simon.techkid@outlook.com>
2024-07-17 21:57:37 -0700

    Version 3.0

--- src/ComparisonPairingsHandler.cs
+++ src/ComparisonPairingsHandler.cs
@@ -8,50 +8,22 @@ using System.Threading.Tasks;
 
 namespace PairingsHandler;
 
-/// <summary>
-/// Handle the creation of a list of pairs of type <typeparamref name="THolder"/> by pairing
-/// a list of objects of type <typeparamref name="TEntry1"/>, each containing values of type <typeparamref name="TData1"/>
-/// and a list of objects of type <typeparamref name="TEntry2"/>, each containing values of type <typeparamref name="TData2"/>.
-/// </summary>
-/// <typeparam name="THolder">The type of the list of pairs.</typeparam>
-/// <typeparam name="TData1">The type of the values of the <typeparamref name="TEntry1"/> objects.</typeparam>
-/// <typeparam name="TData2">The type of the values of the <typeparamref name="TEntry2"/> objects.</typeparam>
-/// <typeparam name="TEntry1">The type of the first object in the pair.</typeparam>
-/// <typeparam name="TEntry2">The type of the second object in the pair.</typeparam>
-/// <remarks>
-/// Create a handler for pairing <typeparamref name="TEntry1"/> information with <typeparamref name="TEntry2"/> information, stored in a list of type <typeparamref name="THolder"/>.
-/// </remarks>
-public abstract partial class PairingsHandler<THolder, TData1, TData2, TEntry1, TEntry2>(StringBroadcaster bcast) :
-    StringBroadcasterBase(bcast),
-    IPairingsHandler<THolder, TData1, TData2, TEntry1, TEntry2>,
+public abstract partial class ComparisonPairingsHandler<THolder, TData1, TData2, TEntry1, TEntry2>(StringBroadcaster bcast) :
+    PairingsHandlerBase<THolder, TData1, TData2, TEntry1, TEntry2>(bcast),
     IComparable<TEntry1, TEntry2>
     where THolder : IPairHolder<TData1, TData2, TEntry1, TEntry2>
     where TEntry1 : IPairable<TData1>
     where TEntry2 : IPairable<TData2>
 {
-    protected List<THolder> Pairs { get; set; } = [];
-
-    public abstract string Name { get; }
-
-    public virtual void CalculatePairings(List<TEntry1> entry1s, List<TEntry2> entry2s) => Pairs = PairPoints(entry1s, entry2s);
-
-    public virtual void CalculatePairings(List<THolder> pairs) => Pairs = pairs;
-
-    /// <summary>
-    /// Create a list of pairs of type <typeparamref name="THolder"/> by pairings lists of type <typeparamref name="TEntry1"/> and of type <typeparamref name="TEntry2"/> objects.
-    /// </summary>
-    /// <param name="entry1s">A list of type <typeparamref name="TEntry1"/> objects.</param>
-    /// <param name="entry2s">A list of type <typeparamref name="TEntry2"/> objects.</param>
-    /// <returns>A list of pairs of type <typeparamref name="THolder"/>, based on the lists of type <typeparamref name="TEntry1"/> and <typeparamref name="TEntry2"/>.</returns>
-    protected List<THolder> PairPoints(List<TEntry1> entry1s, List<TEntry2> entry2s)
+    protected override List<THolder> PairPoints(List<TEntry1> entry1s, List<TEntry2> entry2s)
     {
         int index = 0; // Index of the pairing
-        List<THolder> pairs = new();
+        List<THolder> pairs = [];
 
         foreach (TEntry1 entry1 in entry1s)
         {
-            List<Task> tasks = new()
-            {
+            List<Task> tasks =
+            [
                 Task.Run(() =>
                 {
                     TEntry2 match;
@@ -91,7 +63,7 @@ public abstract partial class PairingsHandler<THolder, TData1, TData2, TEntry1, 
 
                     return GetHolder(entry1, entry2s.First());
                 })
-            };
+            ];
 
             Task[] pairJobs = tasks.ToArray();
 
@@ -116,16 +88,4 @@ public abstract partial class PairingsHandler<THolder, TData1, TData2, TEntry1, 
     public abstract int Compare(TEntry1 entry1, TEntry2 entry2);
 
     protected abstract TEntry2 NarrowDown(TEntry1 element, IList<TEntry2> elements);
-
-    public virtual void FinalizePairings() { }
-
-    public IEnumerator<THolder> GetEnumerator()
-    {
-        return Pairs.GetEnumerator();
-    }
-
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
 }
---
+++ src/PairingsHandlerBase.cs
@@ -0,0 +1,56 @@
+﻿// PairingsHandler by Simon Field
+
+using Logging.Broadcasting;
+using System.Collections.Generic;
+
+namespace PairingsHandler;
+
+/// <summary>
+/// Handle the creation of a list of pairs of type <typeparamref name="THolder"/> by pairing
+/// a list of objects of type <typeparamref name="TEntry1"/>, each containing values of type <typeparamref name="TData1"/>
+/// and a list of objects of type <typeparamref name="TEntry2"/>, each containing values of type <typeparamref name="TData2"/>.
+/// </summary>
+/// <typeparam name="THolder">The type of the list of pairs.</typeparam>
+/// <typeparam name="TData1">The type of the values of the <typeparamref name="TEntry1"/> objects.</typeparam>
+/// <typeparam name="TData2">The type of the values of the <typeparamref name="TEntry2"/> objects.</typeparam>
+/// <typeparam name="TEntry1">The type of the first object in the pair.</typeparam>
+/// <typeparam name="TEntry2">The type of the second object in the pair.</typeparam>
+/// <remarks>
+/// Create a handler for pairing <typeparamref name="TEntry1"/> information with <typeparamref name="TEntry2"/> information, stored in a list of type <typeparamref name="THolder"/>.
+/// </remarks>
+public abstract class PairingsHandlerBase<THolder, TData1, TData2, TEntry1, TEntry2>(StringBroadcaster bcast) :
+    StringBroadcasterBase(bcast),
+    IPairingsHandler<THolder, TData1, TData2, TEntry1, TEntry2>,
+    IEnumerable<THolder>
+    where THolder : IPairHolder<TData1, TData2, TEntry1, TEntry2>
+    where TEntry1 : IPairable<TData1>
+    where TEntry2 : IPairable<TData2>
+{
+    public abstract string Name { get; }
+
+    protected List<THolder> Pairs { get; set; } = [];
+
+    public virtual void CalculatePairings(List<TEntry1> entry1s, List<TEntry2> entry2s) => Pairs = PairPoints(entry1s, entry2s);
+
+    public virtual void CalculatePairings(List<THolder> pairs) => Pairs = pairs;
+
+    /// <summary>
+    /// Create a list of pairs of type <typeparamref name="THolder"/> by pairings lists of type <typeparamref name="TEntry1"/> and of type <typeparamref name="TEntry2"/> objects.
+    /// </summary>
+    /// <param name="entry1s">A list of type <typeparamref name="TEntry1"/> objects.</param>
+    /// <param name="entry2s">A list of type <typeparamref name="TEntry2"/> objects.</param>
+    /// <returns>A list of pairs of type <typeparamref name="THolder"/>, based on the lists of type <typeparamref name="TEntry1"/> and <typeparamref name="TEntry2"/>.</returns>
+    protected abstract List<THolder> PairPoints(List<TEntry1> entry1s, List<TEntry2> entry2s);
+
+    public virtual void FinalizePairings() { }
+
+    public IEnumerator<THolder> GetEnumerator()
+    {
+        return Pairs.GetEnumerator();
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
